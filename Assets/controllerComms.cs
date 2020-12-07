ca18c8c15600e60dba1efed048aa7a1c80523350
Mingye Chen <61096358+mingyeeee@users.noreply.github.com>
2020-12-06 20:20:08 -0500

    Add files via upload

---
+++ Assets/controllerComms.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Bruh : MonoBehaviour
+{
+    SerialPort stream = new SerialPort("COM5", 9600);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("yo");
+        stream.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string value = stream.ReadLine();
+            Debug.Log(value);
+    }
+}
